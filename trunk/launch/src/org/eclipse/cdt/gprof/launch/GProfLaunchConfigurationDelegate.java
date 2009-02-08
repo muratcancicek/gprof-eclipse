5e0b31bbf2196b6926d795ca27b083b3df996786
chrisculy <chrisculy@b62bea14-f0ea-11dd-a295-0b7b0935bf94>
2009-02-08 06:22:33 +0000

    -Initial commit of the launch plugin.

---
+++ trunk/launch/src/org/eclipse/cdt/gprof/launch/GProfLaunchConfigurationDelegate.java
@@ -0,0 +1,33 @@
+/*
+ * =======================================
+ * ============ gprof-eclipse ============
+ * =======================================
+ * 
+ * File: GProfLaunchConfigurationDelegate.java
+ * 
+ * ---------------------------------------
+ * 
+ * Last changed:
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+
+package org.eclipse.cdt.gprof.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class GProfLaunchConfigurationDelegate implements ILaunchConfigurationDelegate
+{
+	
+	public void launch(ILaunchConfiguration arg0, String arg1, ILaunch arg2, IProgressMonitor arg3) throws CoreException
+	{
+		// TODO Auto-generated method stub
+		int x = 45;
+	}
+	
+}
